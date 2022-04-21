f1f7daa0dda04dab57ac54d21a1f1273690c00f9
shogo-115 <67040383+shogo-115@users.noreply.github.com>
2022-04-21 16:15:04 +0900

    Initial commit

---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailB = () => {
+  return (
+    <div>
+      <h1>PAGE1B</h1>
+    </div>
+  );
+};
---
+++ src/Page2.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>PAGE2</h1>
+      <Link to="/page2/100">UrlParameter</Link>
+      <br />
+      <Link to="/page2/100?name=hoge">Query</Link>
+    </div>
+  );
+};
